d572ead9745cc4f889dcdbf232eb7852c117100b
RadoslawM <radoslawmazurr@gmail.com>
2019-05-19 11:50:28 +0200

    add react file shopcart

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './App.css';
+import TopBar from './Topbar';
+import { connect } from 'react-redux';
+import { addItem } from './actions';
+
+
+export const formatPrice = (price) => {
+  return `${(price * 1).toFixed(2)}PLN`;
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TopBar />
+        <h1>Shopping Cart</h1>
+        <div className='itemList'>
+          <ul>
+            {this.props.items.map(product =>
+              <li key={product.id}>
+                <p>{product.name}</p>
+                <p><img src={product.image}></img></p>
+                <p>{formatPrice(product.price)}</p>
+                <button onClick={() => {
+                  this.props.addItem(product.id)
+                }}>
+                  Add to cart
+                </button>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+const mapStateToProps = (store) => ({
+  items: store.items
+})
+const mapDispatchToProps = {
+  addItem
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
